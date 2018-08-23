b412f7562360098a8a5611b7857fb21dfb7e8e52
Kroki1 <36132753+Kroki1@users.noreply.github.com>
2018-08-23 21:55:15 +0200

    Add files via upload

--- app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
@@ -19,6 +19,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 
+import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -47,9 +48,13 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
 
         // Find the TextView in the list_item.xml layout with the ID version_name
         TextView magnitudeTextView = (TextView) listItemView.findViewById(R.id.magTV);
-        // Get the version name from the current AndroidFlavor object and
+
+
+        // Get the version name from the current AndroidFlavor object and format it with one decimal place i.e 0.0
         // set this text on the name TextView
-        magnitudeTextView.setText(Double.toString(currentEq.getMagnitude()));
+        DecimalFormat formatter = new DecimalFormat("0.0");
+        String output = formatter.format(currentEq.getMagnitude());
+        magnitudeTextView.setText(output);
 
         // Find the TextView in the list_item.xml layout with the ID version_number
         TextView primaryLocationTextView = (TextView) listItemView.findViewById(R.id.placePrimaryTV);
